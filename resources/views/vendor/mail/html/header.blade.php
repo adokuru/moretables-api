5697791956666cd99e70bfebac6cf83736543c53
David Adokuru <45779326+adokuru@users.noreply.github.com>
2026-05-12 22:21:30 +0100

    ;ll

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ asset('logo.png') }}" class="logo" alt="{{ trim($slot) !== '' ? trim($slot) : config('app.name') }}">
+<img src="{{ asset('logo.png') }}" class="logo" width="220" height="40" style="width: 220px; height: auto; max-width: 220px; margin-top: 15px; margin-bottom: 10px;" alt="{{ trim($slot) !== '' ? trim($slot) : config('app.name') }}">
 </a>
 </td>
 </tr>
