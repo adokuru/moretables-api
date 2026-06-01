6100a385bde438f7b57366bd1b812856d0ba0921
David Adokuru <45779326+adokuru@users.noreply.github.com>
2026-06-01 09:30:12 +0100

    updates

--- routes/v1/merchant.php
+++ routes/v1/merchant.php
@@ -23,7 +23,6 @@ use App\Http\Controllers\Api\V1\MerchantRestaurantOverviewController;
 use App\Http\Controllers\Api\V1\MerchantRestaurantReviewController;
 use App\Http\Controllers\Api\V1\MerchantRestaurantSettingsController;
 use App\Http\Controllers\Api\V1\MerchantRestaurantSpecialDayController;
-use App\Http\Controllers\Api\V1\MerchantRestaurantSpecialDayController;
 use App\Http\Controllers\Api\V1\MerchantRestaurantStaffController;
 use App\Http\Controllers\Api\V1\MerchantTableCombinationController;
 use App\Http\Controllers\Api\V1\MerchantTableController;
